d2b64cd24f08f5cb24b0eafe6cd5e958457df3b8
Lucas Douglas Reis de Oliveira <LucasDouglas-10@hotmail.com>
2018-08-16 21:35:17 -0300

    Criando classe Controller.

---
+++ Aulas/Aula05_EFDataBaseFirst/Controllers/PersonController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula05_EFDataBaseFirst.Controllers
+{
+    class PersonController
+    {
+        void Inserir(Person p)
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            contexto.Person.Add(p);
+            contexto.SaveChanges();
+        }
+    }
+}
